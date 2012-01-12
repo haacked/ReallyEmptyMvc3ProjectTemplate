2c594946656fb3c20c8993472922e7922523ddef
Tim Heuer <tim@timheuer.com>
2012-01-11 21:37:03 -0800

    Bug fixes in PKGDEF file and template naming

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 cd %~dp0
 cd ReallyEmptyMvcWebApplicationProjectTemplatev3.01.cshtml
 REM create the project template zip
-..\tools\7za.exe a -tzip -mx9 ..\ProjectTemplates\CSharp\Web\1033\ReallyEmptyMvc3ProjectTemplate.zip *
+..\tools\7za.exe a -tzip -mx9 ..\ProjectTemplates\CSharp\Web\ReallyEmptyMvc3ProjectTemplate.zip *
 pushd %~dp0
 REM create the VSIX file based on the zip and manifest data
 tools\7za.exe a -tzip -mx9 ReallyEmptyMvc3ProjectTemplate.vsix @vsixfiles.txt
